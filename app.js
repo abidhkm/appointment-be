22292612e985340b419e02616f2b7d6e7e69764b
Muhammed Abidh <abidh.k@appsintegra.com>
2020-07-05 22:31:52 +0400

    name update

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
